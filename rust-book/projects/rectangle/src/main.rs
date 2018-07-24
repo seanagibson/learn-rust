de18b810387ac6219796f889c98d05b782b59e2d
Sean Gibson <seanandgibson@gmail.com>
2018-07-23 19:35:25 -0700

    Init commit

---
+++ rust-book/projects/rectangle/src/main.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50};
+    let sq_rect = Rectangle::square(25);
+
+    println!("The area of the rectangle is {} square pixels", rect1.area());
+    println!("Square: {:?}", sqRect);
+}
